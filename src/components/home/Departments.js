0924b335a91de0b3bdc4351317619bd3f8f49766
superRaptor911 <raptor.inc2018@gmail.com>
2021-10-03 01:59:05 +0530

    Departments Links and hover effect

--- src/components/home/Departments.js
+++ src/components/home/Departments.js
@@ -9,38 +9,47 @@ const DepartmentList = [
   {
     name: 'CIVIL',
     image: 'CIVIL.png',
+    link: 'https://drive.google.com/folderview?id=1dAS4QbZ83uK2vM2TiWu4g-qf9bacImAF',
   },
   {
     name: 'CSE',
     image: 'CSE.png',
+    link: 'https://drive.google.com/folderview?id=1dHzEW7huX2FXQoIfOJXn_pvQYiUDVxU2',
   },
   {
     name: 'EC',
     image: 'EC.png',
+    link: 'https://drive.google.com/folderview?id=1dHkgiJd2i_BXg7kP-AvGE-bwGhzC18q_',
   },
   {
     name: 'EEE',
     image: 'EEE.png',
+    link: 'https://drive.google.com/folderview?id=1dRVwjwKWoM_7XdIuRkEJNm3kiqQYMVFF',
   },
   {
     name: 'IT',
     image: 'IT.png',
+    link: 'https://drive.google.com/folderview?id=1dHzEW7huX2FXQoIfOJXn_pvQYiUDVxU2',
   },
   {
     name: 'MECH',
     image: 'MECH.png',
+    link: 'https://drive.google.com/folderview?id=1d9Cd-uodczVkL1dTKvvreDhkq-S2889J',
   },
   {
     name: 'MCA',
     image: 'MCA.png',
+    link: '',
   },
   {
     name: 'MATHS',
     image: 'MATHS.png',
+    link: '',
   },
   {
     name: 'GENERAL',
     image: 'GENERAL.png',
+    link: 'https://drive.google.com/folderview?id=1fW5rMAPu-1JWPPoW4cgu4WcklFgZf2xh',
   },
 ];
 
@@ -86,7 +95,12 @@ const Departments = () => {
 
       <div className={css(styles.departments)}>
         {DepartmentList.map((item, id) => (
-          <div className={css(styles.departmentIcon)} key={id}>
+          <div
+            className={css(styles.departmentIcon)}
+            key={id}
+            onClick={() => {
+              window.open(item.link, '_blank');
+            }}>
             <GatsbyImage
               image={getImageFromData(data, item.image)}
               alt={item.name}></GatsbyImage>
@@ -117,6 +131,10 @@ const styles = StyleSheet.create({
   departmentIcon: {
     textAlign: 'center',
     marginTop: 60,
+    ':hover': {
+      transform: 'scale(1.1)',
+      transitionDuration: '0.2s',
+    },
   },
 });
 export default Departments;
