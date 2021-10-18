522005043e230213bba69dcb44f3851198119a41
superRaptor911 <raptor.inc2018@gmail.com>
2021-10-18 15:21:08 +0530

    Header mobile

--- src/.session.vim
+++ src/.session.vim
@@ -8,30 +8,30 @@ if expand('%') == '' && !&modified && line('$') <= 1 && getline(1) == ''
   let s:wipebuf = bufnr('%')
 endif
 set shortmess=aoO
-badd +15 pages/index.js
-badd +2 components/Header.js
+badd +67 pages/index.js
+badd +21 components/Header.js
 badd +27 pages/PostCreator.js
 badd +7 styles/Colors.js
 badd +21 styles/global.css
-badd +34 components/home/HomeText.js
+badd +24 components/home/HomeText.js
 badd +2 components/home/OurServices.js
 badd +62 styles/Typography.js
-badd +46 components/home/HomeMain.js
+badd +30 components/home/HomeMain.js
 badd +34 components/home/AboutUs.js
-badd +1 components/home/Departments.js
+badd +136 components/home/Departments.js
 badd +35 hooks/useDeviceType.js
 badd +11 Utility.js
 argglobal
 %argdel
-edit pages/index.js
+edit components/home/Departments.js
 argglobal
-balt components/Header.js
-let s:l = 8 - ((7 * winheight(0) + 22) / 45)
+balt pages/index.js
+let s:l = 136 - ((44 * winheight(0) + 22) / 45)
 if s:l < 1 | let s:l = 1 | endif
 keepjumps exe s:l
 normal! zt
-keepjumps 8
-normal! 08|
+keepjumps 136
+normal! 031|
 tabnext 1
 if exists('s:wipebuf') && len(win_findbuf(s:wipebuf)) == 0&& getbufvar(s:wipebuf, '&buftype') isnot# 'terminal'
   silent exe 'bwipe ' . s:wipebuf
--- src/components/HeaderMobile.js
+++ src/components/HeaderMobile.js
@@ -3,13 +3,157 @@ import React, {useEffect, useState} from 'react';
 import {StyleSheet, css} from 'aphrodite';
 import {COLORS} from '../styles/Colors';
 import {Link} from 'gatsby';
+import {
+  Dropdown,
+  DropdownToggle,
+  DropdownMenu,
+  DropdownItem,
+} from 'shards-react';
 
-const HeaderMobile = () => {
+import dropdownImg from '../../content/images/dropdown.png';
+import dropdownSelImg from '../../content/images/dropdownSelected.png';
+
+const Links = [
+  {
+    name: 'Home',
+  },
+  {
+    name: 'Department',
+  },
+  {
+    name: 'About Us',
+  },
+];
+
+const MenuLinks = [
+  {
+    name: 'Job Notification',
+    path: '/jobs',
+  },
+  {
+    name: 'Guides',
+    path: '/guides',
+  },
+  {
+    name: 'Quiz',
+    path: '/quiz',
+  },
+];
+
+const GetTitle = ({headerType}) => {
+  if (headerType == 2) {
+    return (
+      <div className={css(styles.name)}>
+        <span style={{color: COLORS.primary}}>Aa</span>
+        <span style={{color: COLORS.text}}>zadi</span>
+      </div>
+    );
+  }
   return (
-    <div>
-      <p>hello gatsby</p>
+    <div className={css(styles.name)}>
+      <span style={{color: COLORS.bg}}>Aa</span>
+      <span style={{color: COLORS.text}}>zadi</span>
     </div>
   );
 };
 
-export default HeaderMobile;
+const Header = () => {
+  const [showMenu, setShowMenu] = useState(false);
+  const [headerType, setHeaderType] = useState(1);
+  const toggeleMenu = () => {
+    setShowMenu(!showMenu);
+  };
+
+  useEffect(() => {
+    window.addEventListener('scroll', onScroll);
+
+    return () => {
+      window.removeEventListener('scroll', onScroll);
+    };
+  }, []);
+
+  const onScroll = () => {
+    if (window.scrollY > 100) {
+      setHeaderType(2);
+    } else {
+      setHeaderType(1);
+    }
+  };
+
+  return (
+    <div className={css(styles.root, headerType == 2 ? styles.colorBg : null)}>
+      <GetTitle headerType={headerType} />
+      <div className={css(styles.linkContainer)}>
+        <Dropdown open={showMenu} toggle={toggeleMenu} className="d-table">
+          {showMenu ? (
+            <img src={dropdownSelImg} alt="menu" onClick={toggeleMenu} />
+          ) : (
+            <img src={dropdownImg} alt="menu" onClick={toggeleMenu} />
+          )}
+          <DropdownMenu right>
+            {MenuLinks.map((item, id) => (
+              <DropdownItem key={id}>
+                <Link to={item.path} className={css(styles.text)}>
+                  {item.name}
+                </Link>
+              </DropdownItem>
+            ))}
+          </DropdownMenu>
+        </Dropdown>
+      </div>
+    </div>
+  );
+};
+
+const styles = StyleSheet.create({
+  root: {
+    position: 'fixed',
+    width: '100vw',
+    display: 'flex',
+    paddingTop: 10,
+    paddingBottom: 20,
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    zIndex: 9099,
+  },
+  colorBg: {
+    backgroundColor: COLORS.bg,
+    boxShadow: '0 4px 2px -2px gray',
+  },
+  editor: {
+    width: '50%',
+  },
+  name: {
+    marginLeft: 20,
+    fontFamily: 'Spartan',
+    fontStyle: 'normal',
+    fontWeight: '600',
+    fontSize: '28.8px',
+    lineHeight: '99.4%',
+    color: '#000000',
+  },
+  linkContainer: {
+    display: 'flex',
+    marginRight: 15,
+    alignItems: 'center',
+  },
+  linkItem: {
+    fontFamily: 'Spartan',
+    fontStyle: 'normal',
+    fontWeight: 'normal',
+    fontSize: '24px',
+    lineHeight: '27px',
+    color: '#000000',
+    marginRight: 52,
+  },
+  text: {
+    fontFamily: 'Spartan',
+    fontStyle: 'normal',
+    fontWeight: 'normal',
+    fontSize: '26.3016px',
+    lineHeight: '29px',
+    color: '#000000',
+  },
+});
+
+export default Header;
